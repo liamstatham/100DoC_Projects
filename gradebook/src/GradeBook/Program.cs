106d86adec2325fc224d91678f3696ae6bb14a09
liam statham <liamstatham@gmail.com>
2021-02-12 17:56:28 +0000

    c# beginings!

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var numbers = new double[3];
+            numbers[0] = 11.3;
+            numbers[1] = 12.3;
+            numbers[2] = 20.3;
+
+            var result = numbers[0] + numbers[1] + numbers[2];
+            System.Console.WriteLine(result);
+            if (args.Length <0) 
+            {
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello.");
+            }
+        }
+    }
+}
