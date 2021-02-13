62e6256eafedf10f6d208f83bdf41174d9c7bde2
liam statham <liamstatham@gmail.com>
2021-02-13 20:09:07 +0000

    c#

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -7,12 +8,21 @@ namespace GradeBook
         static void Main(string[] args)
         {
 
-            var numbers = new double[3];
-            numbers[0] = 11.3;
-            numbers[1] = 12.3;
-            numbers[2] = 20.3;
+            var grades = new List<double>() {11.3, 12.3, 20.3, 100.00};
+            grades.Add(13.44);
 
-            var result = numbers[0] + numbers[1] + numbers[2];
+
+            var result = 0.0;
+            var count = 0;
+
+            foreach(var number in grades)
+            {
+                result += number;
+                count += 1;
+            }
+            var average = result / grades.Count;
+
+            System.Console.WriteLine($"The average is {average:N1}");
             System.Console.WriteLine(result);
             if (args.Length <0) 
             {
