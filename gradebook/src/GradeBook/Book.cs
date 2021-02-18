e76f74a2d3a0a8160ff24b060c6fb2ecf740851f
Liam Statham <liamstatham@gmail.com>
2021-02-18 20:28:28 +0000

    Error handling

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -10,6 +10,26 @@ namespace GradeBook
                 grades = new List<double>();
                 Name = name;
             }
+
+            public void AddLetterGrade(char letter)
+            {
+                switch (letter) 
+                {
+                    case 'A':
+                        AddGrade(90);
+                        break;
+                    case 'B':
+                        AddGrade(80);
+                        break;
+                    case 'C':
+                        AddGrade(70);
+                        break;                    
+                    default:
+                        AddGrade(0);
+                        break; 
+                }
+            }
+            
             public void AddGrade(double grade)
             {
                 if(grade <= 100 && grade >= 0)
@@ -18,7 +38,7 @@ namespace GradeBook
                 }
                 else
                 {
-                    System.Console.WriteLine("Invalid value");
+                    throw new ArgumentException($"Invalid {nameof(grade)}");
                 }
                 
             }
@@ -38,6 +58,25 @@ namespace GradeBook
                         result.Average += grades[index];
                     } 
                 result.Average /= grades.Count;
+
+                switch(result.Average)
+                {
+                    case var d when d >= 90.0:
+                        result.Letter = 'A';
+                        break;
+                    case var d when d >= 80.0:
+                        result.Letter = 'B';
+                        break;
+                    case var d when d >= 70.0:
+                        result.Letter = 'C';
+                        break;
+                    case var d when d >= 60.0:
+                        result.Letter = 'D';
+                        break;
+                    default:
+                        result.Letter = 'F';
+                        break;
+                }
 
                 return result;
             }
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -8,14 +8,40 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("Liam's GradeBook");
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
             
+            var done = false;
+
+            while (!done)
+            {
+                System.Console.WriteLine("Please enter a grade or 'q' to quit.");
+                var input = Console.ReadLine();
+
+                if(input =="q")
+                {
+                    done = true;
+                    continue;
+                }
+                try
+                {
+                var grade = double.Parse(input);
+                book.AddGrade(grade);
+                }
+                catch(ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                
+            }
+
             var stats = book.GetStatistics();
             System.Console.WriteLine($"The highest grade is {stats.High}");
             System.Console.WriteLine($"The average is {stats.Average:N1}");
             System.Console.WriteLine($"The lowest grade is {stats.Low}");
+            System.Console.WriteLine($"The letter grade is {stats.Letter}");
         }
     }
 
