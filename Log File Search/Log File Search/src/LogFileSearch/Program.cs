3f13b59c511c72013b373a90bfcc0fc1bd9d44f9
Liam Statham <liamstatham@gmail.com>
2021-03-01 20:37:21 +0000

    Unit testing!

---
+++ Log File Search/Log File Search/src/LogFileSearch/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Log_File_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Please type the log file name:");
+            var fname = Console.ReadLine();
+            var file = new OpenFile();
+
+            file.fhand(fname);
+            file.AddToCsv();
+        }
+    }
+}
