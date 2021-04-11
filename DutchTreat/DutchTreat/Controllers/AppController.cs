9bd40eb76eb7520a52f189db129370f97ff72296
Liam Statham <liamstatham@gmail.com>
2021-04-11 15:31:13 +0100

    Added services and logging for email sending service from form

--- DutchTreat/DutchTreat/Controllers/AppController.cs
+++ DutchTreat/DutchTreat/Controllers/AppController.cs
@@ -1,4 +1,5 @@
-﻿using DutchTreat.ViewModels;
+﻿using DutchTreat.Services;
+using DutchTreat.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,12 @@ namespace DutchTreat.Controllers
 {
     public class AppController : Controller 
     {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
         public IActionResult Index()
         {
             //throw new InvalidProgramException("bad things happen to good developers");
@@ -22,16 +29,15 @@ namespace DutchTreat.Controllers
 
         }
 
-        [HttpPost]
+        [HttpPost("contact")]
         public IActionResult Contact(ContactViewModel model)
         {
             if(ModelState.IsValid)
             {
                 // send email
-            }
-            else
-            {
-                // return error
+                _mailService.SendMessage("liamstatham@gmail.com", model.Subject, $"From: {model.Name} - {model.Email}, Meessage: {model.Message}");
+                ViewBag.UserMessage = "Mail sent.";
+                ModelState.Clear();
             }
             return View();
         }
--- DutchTreat/DutchTreat/Startup.cs
+++ DutchTreat/DutchTreat/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using DutchTreat.Services;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,6 +17,8 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IMailService, NullMailService>();
+
             services.AddControllersWithViews()
                 .AddRazorRuntimeCompilation();
 
