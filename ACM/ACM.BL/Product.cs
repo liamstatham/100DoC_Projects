682859a372b480e8126e6c4b98fd4bd800819099
Liam Statham <liamstatham@gmail.com>
2021-06-07 20:57:00 +0100

    Building reusable components - code library

--- ACM/ACM.BL/Product.cs
+++ ACM/ACM.BL/Product.cs
@@ -18,10 +18,21 @@ namespace ACM.BL
         }
 
         public int ProductId { get; private set; }
-        public string ProductName { get; set; }
         public string ProductDescription { get; set; }
         //decimal? can be null
         public decimal? CurrentPrice { get; set; }
+        private string _productName;
+        public string ProductName
+        {
+            get
+            {
+                return _productName;
+            }
+            set
+            {
+                _productName = value;
+            }
+        }
 
         public override string ToString() => ProductName;
 
