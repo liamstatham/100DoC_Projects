f4bbbb0f2bfdd502cf318304b41adbb088b51523
Liam Statham <liamstatham@gmail.com>
2021-06-17 20:13:45 +0100

    Start of new course.

---
+++ HealthAssist/HealthAssist/App_Start/RouteConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace HealthAssist
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+
+            Route myRoute = new Route("{controller}/{action}/{id}",
+                new RouteValueDictionary { { "controller", "Home" }, { "action", "Index" }, { "id", "1" } },
+                new MvcRouteHandler());
+            routes.Add(myRoute);
+        }
+    }
+}
---
+++ HealthAssist/HealthAssist/Global.asax.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace HealthAssist
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_BeginRequest()
+        {
+            Debug.WriteLine("Begin Request");
+        }
+        protected void Application_MapRequestHandler()
+        {
+            Debug.WriteLine("Map Handler");
+        }
+        protected void Application_PostMapRequestHandler()
+        {
+            Debug.WriteLine("Post map handler");
+        }
+        protected void Application_AquireRequestState()
+        {
+            Debug.WriteLine("Request state");
+        }
+        protected void Application_PreRequestHandlerExecute()
+        {
+            Debug.WriteLine("Pre handler execute");
+        }
+        protected void Application_PostRequestHandlerExecute()
+        {
+            Debug.WriteLine("Post handler execute");
+        }
+        protected void Application_EndRequest()
+        {
+            Debug.WriteLine("End Request");
+        }
+    }
+}
