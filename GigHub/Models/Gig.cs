44ecdeea710578eaffd8b4fb6e1e988e33e73df0
Liam Statham <liamstatham@gmail.com>
2021-06-28 19:48:03 +0100

    Added gig and genre tables to database, split Identity user to AppUser and Db context classes.

---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.Models
+{
+    public class Gig
+    {
+        //who
+        //when
+        //where
+        //genre
+
+        //Properties
+        public int Id { get; set; }
+        public ApplicationUser Artist { get; set; }
+        public DateTime DateTime { get; set; }
+        public String Venue { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
