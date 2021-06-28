559f17e4388f890868f067c0aa2725f466c9b1b2
Liam Statham <liamstatham@gmail.com>
2021-06-28 19:58:34 +0100

    Added data annotations to Gig and Genre classes, to remove nullable cols.

--- GigHub/Models/Gig.cs
+++ GigHub/Models/Gig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -14,9 +15,17 @@ namespace GigHub.Models
 
         //Properties
         public int Id { get; set; }
+
+        [Required]
         public ApplicationUser Artist { get; set; }
+
         public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public String Venue { get; set; }
+
+        [Required]
         public Genre Genre { get; set; }
     }
 }
