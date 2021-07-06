230e212474b0a315c80d85405c3757703dfefad5
Liam Statham <liamstatham@gmail.com>
2021-07-06 16:11:31 +0100

    More assert: string and collections

---
+++ MyClassesTest/StringAssertClassTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class StringAssertClassTest : TestBase
+    {
+        [TestMethod]
+        public void ContainsTest()
+        {
+            string str1 = "Liam Statham";
+            string str2 = "Statham";
+
+            StringAssert.Contains(str1, str2);
+
+        }
+
+        [TestMethod]
+        public void StartsWith()
+        {
+            string str1 = "This starts with";
+            string str2 = "This doesn't";
+
+            StringAssert.StartsWith(str1, str2);
+        }
+
+        [TestMethod]
+        public void IsAllLowerCase()
+        {
+            Regex r = new Regex(@"^([^A-Z])+$");
+
+            StringAssert.Matches("all lower case", r);
+        }
+
+        [TestMethod]
+        public void IsNotAllLowerCase()
+        {
+            Regex r = new Regex(@"^([^A-Z])+$");
+
+            StringAssert.DoesNotMatch("All Lower Case", r);
+        }
+    }
+}
