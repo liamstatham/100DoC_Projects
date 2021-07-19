8262bdef93745c1611cdf8aa8af9a25acac732f8
Liam Statham <liamstatham@gmail.com>
2021-07-19 15:54:25 +0100

    Good methods module

--- CSharpBP-Basics/AcmeApp/Acme.Biz/Vendor.cs
+++ CSharpBP-Basics/AcmeApp/Acme.Biz/Vendor.cs
@@ -17,6 +17,38 @@ namespace Acme.Biz
         public string Email { get; set; }
 
         /// <summary>
+        /// Sends a product to the vendor.
+        /// </summary>
+        /// <param name="product">Product to order.</param>
+        /// <param name="quantity">Quantity of the product to order.</param>
+        /// <returns></returns>
+        public OperationResult PlaceOrder(Product product, int quantity)
+        {
+            if (product === null)
+                throw new ArgumentNullException(nameof(product));
+            if (quantity <= 0)
+                throw new ArgumentOutOfRangeException(nameof(quantity));
+
+            var success = false;
+
+            var orderText = "Order from Acme, Inc" + System.Environment.NewLine +
+                            "Product: " + product.ProductCode +
+                                                     System.Environment.NewLine +
+                            "Quantity: " + quantity;
+
+            var emailService = new EmailService();
+            var confirmation = emailService.SendMessage("New Order", orderText, this.Email);
+
+            if (confirmation.StartsWith("Message sent:"))
+            {
+                success = true;
+            }
+            var operationResult = new OperationResult(success, orderText);
+            return operationResult;
+        }
+
+
+        /// <summary>
         /// Sends an email to welcome a new vendor.
         /// </summary>
         /// <returns></returns>
